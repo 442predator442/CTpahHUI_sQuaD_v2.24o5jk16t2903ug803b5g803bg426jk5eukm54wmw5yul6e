b97fc04f18102a2cb1b984d9ba198f9cf0567d24
CTpaHHui_KOT <58422307+442predator442@users.noreply.github.com>
2020-08-14 11:46:34 +0300

    Add files via upload

--- cogs/partner.py
+++ cogs/partner.py
@@ -17,23 +17,37 @@ class partner(commands.Cog):
 		schannel = guild.get_channel(738499674784137298)
 		nchannel = guild.get_channel(738499742350180474)
 		bchannel = guild.get_channel(738499812625743912)
-		prole = guild.get_role(738500276742389850)
+		pch = guild.get_channel(743750212551704596)
 		message = ctx.message.content.split()
-		if amount < 100:
-			if amount < 40:
-				web = await schannel.create_webhook(name = partner.name)
-				message = ctx.message.content.split()
-				for elem in message:
-					if elem.startswith("https://") or elem.startswith("http://"):
-						embed = discord.Embed(title = 'Приесоденится на сервер (нажми)', url = f'{elem}', description = f'**Партнер:** {partner.mention}\n\n**Текст:**\n{text}')
-						embed.set_footer(text = f'ПАРТНЕРКА')
-						await web.send(embed = embed, avatar_url = partner.avatar_url)
-				await web.delete()
-				e = discord.Embed(title = '<:check:738641498127597599> Партнерка успешно совершена.', colour = discord.Colour.green())
-				await ctx.send(embed = e)
-				await partner.add_roles(prole)
-				await partner.send(f'**Ваша партнерка отправлена на сервере `Странный SQUAD`**\n**Вашу партнерку вы можете найти сдесь: {schannel}**')
-			else:
+		if ctx.channel ==pch:
+			if amount < 100:
+				if amount < 40:
+					web = await schannel.create_webhook(name = partner.name)
+					message = ctx.message.content.split()
+					for elem in message:
+						if elem.startswith("https://") or elem.startswith("http://"):
+							embed = discord.Embed(title = 'Приесоденится на сервер (нажми)', url = f'{elem}', description = f'**Партнер:** {partner.mention}\n\n**Текст:**\n{text}')
+							embed.set_footer(text = f'ПАРТНЕРКА')
+							await web.send(embed = embed, avatar_url = partner.avatar_url)
+					await web.delete()
+					e = discord.Embed(title = '<:check:738641498127597599> Партнерка успешно совершена.', colour = discord.Colour.green())
+					await ctx.send(embed = e)
+					await partner.add_roles(prole)
+					await partner.send(f'**Ваша партнерка отправлена на сервере `Странный SQUAD`**\n**Вашу партнерку вы можете найти сдесь: {schannel}**')
+				else:
+					message = ctx.message.content.split()
+					for elem in message:
+						if elem.startswith("https://") or elem.startswith("http://"):
+							embed = discord.Embed(title = 'Приесоденится на сервер (нажми)', url = f'{elem}', description = f'**Партнер:** {partner.mention}\n\n**Текст:**\n{text}')
+							embed.set_footer(text = f'ПАРТНЕРКА | {time}')
+							embed.set_author(name = f'{partner.name}#{partner.discriminator}', icon_url = partner.avatar_url)
+							msg = await schannel.send(embed = embed)
+							await msg.edit(content = f'**Пинг:** @here', embed = embed)
+							e = discord.Embed(title = '<:check:738641498127597599> Партнерка успешно совершена.', colour = discord.Colour.green())
+							await ctx.send(embed = e)
+							await partner.add_roles(prole)
+							await partner.send(f'**Ваша партнерка отправлена на сервере `Странный SQUAD`**\n**Вашу партнерку вы можете найти сдесь: {schannel}**')
+			elif amount ==100:
 				message = ctx.message.content.split()
 				for elem in message:
 					if elem.startswith("https://") or elem.startswith("http://"):
@@ -46,51 +60,35 @@ class partner(commands.Cog):
 						await ctx.send(embed = e)
 						await partner.add_roles(prole)
 						await partner.send(f'**Ваша партнерка отправлена на сервере `Странный SQUAD`**\n**Вашу партнерку вы можете найти сдесь: {schannel}**')
-		elif amount ==100:
-			message = ctx.message.content.split()
-			for elem in message:
-				if elem.startswith("https://") or elem.startswith("http://"):
-					embed = discord.Embed(title = 'Приесоденится на сервер (нажми)', url = f'{elem}', description = f'**Партнер:** {partner.mention}\n\n**Текст:**\n{text}')
-					embed.set_footer(text = f'ПАРТНЕРКА | {time}')
-					embed.set_author(name = f'{partner.name}#{partner.discriminator}', icon_url = partner.avatar_url)
-					msg = await schannel.send(embed = embed)
-					await msg.edit(content = f'**Пинг:** @here', embed = embed)
-					e = discord.Embed(title = '<:check:738641498127597599> Партнерка успешно совершена.', colour = discord.Colour.green())
-					await ctx.send(embed = e)
-					await partner.add_roles(prole)
-					await partner.send(f'**Ваша партнерка отправлена на сервере `Странный SQUAD`**\n**Вашу партнерку вы можете найти сдесь: {schannel}**')
 
-		elif amount > 100 < 700:
-			
-			message = ctx.message.content.split()
-			for elem in message:
-				if elem.startswith("https://") or elem.startswith("http://"):
-					embed = discord.Embed(title = 'Приесоденится на сервер (нажми)', url = f'{elem}', description = f'**Партнер:** {partner.mention}\n\n**Текст:**\n{text}')
-					embed.set_footer(text = f'ПАРТНЕРКА | {time}')
-					embed.set_author(name = f'{partner.name}#{partner.discriminator}', icon_url = partner.avatar_url)
-					msg = await nchannel.send(embed = embed)
-					await msg.edit(content = f'@everyone @here', embed = embed)
-					e = discord.Embed(title = '<:check:738641498127597599> Партнерка успешно совершена.', colour = discord.Colour.green())
-					await ctx.send(embed = e)
-					await partner.add_roles(prole)
-					await partner.send(f'**Ваша партнерка отправлена на сервере `Странный SQUAD`**\n**Вашу партнерку вы можете найти сдесь: {nchannel}**')
-		elif amount > 700:
-			
-			message = ctx.message.content.split()
-			for elem in message:
-				if elem.startswith("https://") or elem.startswith("http://"):
-					embed = discord.Embed(title = 'Приесоденится на сервер (нажми)', url = f'{elem}', description = f'**Партнер:** {partner.mention}\n\n**Текст:**\n{text}')
-					embed.set_footer(text = f'ПАРТНЕРКА | {time}')
-					embed.set_author(name = f'{partner.name}#{partner.discriminator}', icon_url = partner.avatar_url)
-					msg = await bchannel.send(embed = embed)
-					await msg.edit(content = f'@everyone @here', embed = embed)
-					e = discord.Embed(title = '<:check:738641498127597599> Партнерка успешно совершена.', colour = discord.Colour.green())
-					await ctx.send(embed = e)
-					await partner.add_roles(prole)
-					await partner.send(f'**Ваша партнерка отправлена на сервере `Странный SQUAD`**\n**Вашу партнерку вы можете найти сдесь: {bchannel}**')
-	else:
-		embed = discord.Embed(title = '<:xmark:738641026209546251> Недостаточно прав использовать данную команду!', colour = discord.Colour.green())
-		await ctx.send(embed = embed)
+			elif amount > 100 < 700:
+				
+				message = ctx.message.content.split()
+				for elem in message:
+					if elem.startswith("https://") or elem.startswith("http://"):
+						embed = discord.Embed(title = 'Приесоденится на сервер (нажми)', url = f'{elem}', description = f'**Партнер:** {partner.mention}\n\n**Текст:**\n{text}')
+						embed.set_footer(text = f'ПАРТНЕРКА | {time}')
+						embed.set_author(name = f'{partner.name}#{partner.discriminator}', icon_url = partner.avatar_url)
+						msg = await nchannel.send(embed = embed)
+						await msg.edit(content = f'@everyone @here', embed = embed)
+						e = discord.Embed(title = '<:check:738641498127597599> Партнерка успешно совершена.', colour = discord.Colour.green())
+						await ctx.send(embed = e)
+						await partner.add_roles(prole)
+						await partner.send(f'**Ваша партнерка отправлена на сервере `Странный SQUAD`**\n**Вашу партнерку вы можете найти сдесь: {nchannel}**')
+			elif amount > 700:
+				
+				message = ctx.message.content.split()
+				for elem in message:
+					if elem.startswith("https://") or elem.startswith("http://"):
+						embed = discord.Embed(title = 'Приесоденится на сервер (нажми)', url = f'{elem}', description = f'**Партнер:** {partner.mention}\n\n**Текст:**\n{text}')
+						embed.set_footer(text = f'ПАРТНЕРКА | {time}')
+						embed.set_author(name = f'{partner.name}#{partner.discriminator}', icon_url = partner.avatar_url)
+						msg = await bchannel.send(embed = embed)
+						await msg.edit(content = f'@everyone @here', embed = embed)
+						e = discord.Embed(title = '<:check:738641498127597599> Партнерка успешно совершена.', colour = discord.Colour.green())
+						await ctx.send(embed = e)
+						await partner.add_roles(prole)
+						await partner.send(f'**Ваша партнерка отправлена на сервере `Странный SQUAD`**\n**Вашу партнерку вы можете найти сдесь: {bchannel}**')
 
 
 def setup(bot):
